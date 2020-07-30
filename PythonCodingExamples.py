9e2700dcaa1290a440b1beb96ed81b5736a6904d
Robert <47223029+robertnochez@users.noreply.github.com>
2020-07-30 00:01:01 -0700

    Update PythonCodingExamples.py

--- PythonCodingExamples.py
+++ PythonCodingExamples.py
@@ -1,5 +1,7 @@
 """Python Coding Examples by Robert Nochez (Last update: 7/29/2020)"""
 
+################################################################################
+
     """ The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation.
     We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
